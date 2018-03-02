8af3ed308e194ad243b94faa635b83822f57ba2e
jacob <jbutler8@terpmail.umd.edu>
2018-03-02 09:11:03 -0800

    updating state with updates to input

--- index.js
+++ index.js
@@ -1,27 +1,31 @@
-let state = {};
+var state = {};
 
-let person = {
+var person = {
   age: 0,
   relationship: null,
   smoker: false
 };
 
-this.state = this.state.bind(this);
-this.person = this.person.bind(this);
+// this.state = this.state.bind(this);
+// this.person = this.person.bind(this);
 
 let age = document.getElementsByName('age');
 let rel = document.getElementsByName('rel');
 let smoker = document.getElementsByName('smoker');
 
 document.addEventListener('input', () => {
-  person.age = age.innerHtml;
-  person.relationship = rel.innerHtml;
-  person.smoker = smoker.innerHtml;
+  person.age = document.getElementsByName('age')[0].value;
+  person.relationship = document.getElementsByName('rel')[0].value;
+  if (document.querySelector('input[type=checkbox]').checked) {
+    person.smoker = true;
+  } else {
+    person.smoker = false;
+  }
   console.log(person);
+
 });
 
 document.addEventListener('select', () => {
-
 });
 
 document.addEventListener('submit', () => {
