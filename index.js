c0d75717f55986dcf8cd28f2238c0c77a8b94093
jacob <jbutler8@terpmail.umd.edu>
2018-03-02 14:26:38 -0800

    add comments and add button functionality

--- index.js
+++ index.js
@@ -1,37 +1,41 @@
+// State object will hold all of the people in the house.
 var state = {};
 
+// initial state of a household member
 var person = {
   age: 0,
   relationship: null,
   smoker: false
 };
 
-// this.state = this.state.bind(this);
-// this.person = this.person.bind(this);
-
-let age = document.getElementsByName('age');
-let rel = document.getElementsByName('rel');
-let smoker = document.getElementsByName('smoker');
-
+// Updates person object dynamically as user inputs data.
 document.addEventListener('input', () => {
   person.age = document.getElementsByName('age')[0].value;
   person.relationship = document.getElementsByName('rel')[0].value;
-  if (document.querySelector('input[type=checkbox]').checked) {
-    person.smoker = true;
-  } else {
-    person.smoker = false;
-  }
-  console.log(person);
-
+  person.smoker = document.querySelector('input[type=checkbox]').checked ?
+      person.smoker = true : person.smoker = false;
 });
 
-document.addEventListener('select', () => {
+// Adds person to state
+document.getElementsByClassName('add')[0].addEventListener('click', () => {
+  addPersonToState(person);
 });
 
-document.addEventListener('submit', () => {
+// When user clicks 'add' button, the current person is added to state.
+function addPersonToState() {
+  const personNum = Object.keys(state).length;
+  state.push = {
+    age: person.age,
+    relationship: person.relationship,
+    smoker: person.smoker
+  };
+  resetPerson();
+}
 
-});
-
-function addToHTML() {
-
+// Resets the person variable to its initial state so that user can add
+// another household member
+function resetPerson() {
+  person.age = 0;
+  person.relationship = null;
+  person.smoker = false;
 }
