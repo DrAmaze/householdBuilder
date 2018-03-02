73149b043bb6931df0aeb41042ceb0f6deeaa459
jacob <jbutler8@terpmail.umd.edu>
2018-03-01 17:17:12 -0800

    create state object

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-document.addEventListener("input", () => {
-  
+let state = {};
+
+let person = {
+  age: 0,
+  relationship: null,
+  smoker: false
+};
+
+document.addEventListener('input', () => {
+
+});
+
+document.addEventListener('select', () => {
+
 });
 
 document.addEventListener('submit', () => {
