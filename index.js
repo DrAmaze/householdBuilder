e88711de63048675d04effaeee4820976852c696
jacob <jbutler8@terpmail.umd.edu>
2018-03-05 11:04:18 -0800

    Update comments and refactor for clarity

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
-// State object will hold all of the people in the house.
+// This file houses all of the functionality logic for this form.
+
+// Global Variables
+
+// State object will hold all of the people in the house. This utilizes
+// browser's local storage to have data persist through page refresh.
 let oldState = localStorage.getItem('householdBuilderState');
 let state = oldState ? JSON.parse(oldState) : { people: {} };
 
-// initial state of a household member
+// initial state of a household member. This dynamically updates with
+// user input.
 let person = {
   age: 0,
   relationship: null,
   smoker: false
 };
 
+// Instantiate the list of household members.
 // Add the current household members to the page upon rendering DOM.
 renderState();
 
+// Event Listeners.
+// There are 4 event listeners on this page. This could be optimized by
+// adding a single event listener to the form, then parsing the specific
+// action, in order to determine the functionality. The current solution
+// is written clearly and (due to the limited functionality of the page)
+// does not pay a costly price for the event listeners.
+
+// Upon form submission, this converts the household to JSON and appends
+// the data to the <pre> tag. This JSON persists as the household is
+// updated and can be resubmitted.
 document.addEventListener('submit', (e) => {
   e.preventDefault();
   let household = JSON.stringify(state);
@@ -29,6 +46,9 @@ document.addEventListener('input', () => {
   person.relationship = document.getElementsByName('rel')[0].value;
 });
 
+// Enables the user to remove people from the household after they are
+// added. This leverages the UUID number in order to delete the
+// appropriate person. It then updates local storage appropriately.
 document.getElementsByClassName('household')[0].addEventListener('click', (e) => {
   e.preventDefault();
   let buttonId = e.target.parentElement.id;
@@ -40,7 +60,10 @@ document.getElementsByClassName('household')[0].addEventListener('click', (e) =>
   updateLocalStorage();
 });
 
-// Adds person to state
+// Validates input based on the prompt's specifications.  If valid,
+// the person object is added to the to state object with a UUID. If
+// invalid, an error DIV is added to the form displaying the user's
+// input errors.
 document.getElementsByClassName('add')[0].addEventListener('click', (e) => {
   e.preventDefault();
   // Variable is used to ensure both age and relationship are validated
@@ -56,9 +79,14 @@ document.getElementsByClassName('add')[0].addEventListener('click', (e) => {
   if (valid) addPersonToState() ;
 });
 
-// When user clicks 'add' button, the current person is added to state.
-function addPersonToState() {
+// Functions that define how the page operates.
 
+// When user clicks 'add' button, the current person is added to state
+// with a unique ID number. The browser's local storage is updated, the
+// person is rendered to the list of people, the person object is reset
+// to its initial state, and finally, the error DIV is removed.
+function addPersonToState() {
+  // operation to determine whether or not the person is a smoker.
   person.smoker = document.querySelector('input[type=checkbox]').checked ?
       person.smoker = true : person.smoker = false;
 
@@ -77,12 +105,19 @@ function addPersonToState() {
   resetError();
 }
 
+// Rendering functions
+
+// Renders the list of household people when DOM loads.
 function renderState() {
   Object.keys(state.people).forEach((key) => {
     renderPerson(key);
   });
 }
 
+// Renders the person to the <ol> through their UUID. This function
+// grabs the most up to date input from the person object and creates a
+// new <li> which is then appended to the current <ol>. This function
+// makes a call to add a delete button to each <li> as well.
 function renderPerson(uuid) {
   let list = document.getElementsByClassName('household')[0];
   let li = document.createElement('LI');
@@ -99,6 +134,8 @@ function renderPerson(uuid) {
   list.appendChild(li);
 }
 
+// This handles the logic for creating a delete button for each
+// person added to the household.
 function addDeleteButton(item) {
   let deleteBtn = document.createElement('BUTTON');
   deleteBtn.setAttribute('class', 'delete');
@@ -107,10 +144,15 @@ function addDeleteButton(item) {
   item.appendChild(deleteBtn);
 }
 
+// Data Entry validations
+
+// Validates the input age is a number and it is greater than 0 (as
+// defined by the given specifications).
 function validateAge() {
   return person.age > 0 ? true : false;
 }
 
+// Validates the input relationship exists.
 function validateRelationship() {
   // This array specifies the types of relationships allowed in a
   // household. This validation is precise due to the precise nature of
@@ -127,17 +169,27 @@ function validateRelationship() {
 }
 
 // Resets the person variable to its initial state so that user can add
-// another household member
+// another household person.
 function resetPerson() {
   person.age = 0;
   person.relationship = null;
   person.smoker = false;
 }
 
+// Error handling
+
+// Handles the logic for error message rendering by adding an error
+// <div> to the <form> in order to display the appropriate error message
 function createErrorDiv(validAge, validRel) {
+  // Resets the error div each time so that the message is relevant to
+  // the latest attempted submission.
   resetError();
+
+  // Creation of the <div>
   let error = document.createElement('DIV');
   error.setAttribute('class', 'error');
+
+  // error messaging logic
   if (!validAge && !validRel) {
     error.innerHTML = 'Age must be a number greater than 0; Relationship must be selected from the options below';
   } else if (!validAge) {
@@ -149,6 +201,7 @@ function createErrorDiv(validAge, validRel) {
   document.getElementsByTagName('FORM')[0].appendChild(error);
 }
 
+// Resets the appended error <div>
 function resetError() {
   if (document.getElementsByClassName('error').length > 0) {
     const error = document.getElementsByClassName('error')[0];
@@ -156,6 +209,8 @@ function resetError() {
   }
 }
 
+// Ensures the user's data persists through DOM re-render by utilizing
+// the browser's local storage.
 function updateLocalStorage() {
   const persistState = JSON.stringify(state);
   localStorage.setItem('householdBuilderState', persistState);
